bb552194bb8ab62f40742d064e57376fa2af2b53
maithanhtrong1113 <maithanhtrong1113@gmail.com>
2023-04-01 15:20:39 +0700

    add font-global

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,20 @@ import "bootstrap/dist/css/bootstrap.css";
 import "../styles/globals.css";
 import { Provider, useSelector } from "react-redux";
 import store from "../store/store";
-
 import { persistor } from "../store/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { useEffect } from "react";
 import "react-toastify/dist/ReactToastify.css";
 import "react-datepicker/dist/react-datepicker.css";
 import Head from "next/head";
+import { Poppins } from "@next/font/google";
+
+const inter = Poppins({ subsets: ["latin"], weight: ["500"] });
 export default function MyApp({ Component, pageProps }) {
   useEffect(() => {
     import("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
+
   return (
     <>
       <Head>
@@ -20,7 +23,9 @@ export default function MyApp({ Component, pageProps }) {
       </Head>
       <Provider store={store}>
         <PersistGate persistor={persistor}>
-          <Component {...pageProps} />
+          <main className={inter.className}>
+            <Component {...pageProps} />
+          </main>
         </PersistGate>
       </Provider>
     </>
