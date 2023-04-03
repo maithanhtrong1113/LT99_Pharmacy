f9adb9f791cd995d386107a4887934e8f5b5f213
maithanhtrong1113 <maithanhtrong1113@gmail.com>
2023-04-03 12:42:17 +0700

    mess

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://lt-99-pharmacy.vercel.app/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,32 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <div id="fb-root"></div>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v16.0'
+        });
+      };
+           (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+            `,
+          }}
+        />
+
+        <div
+          className="fb-customerchat"
+          attribution="biz_inbox"
+          page_id="110305482000429"
+        ></div>
       </body>
     </Html>
   );
