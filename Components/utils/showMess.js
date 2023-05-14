e0ca86b9aa1096567958fb576f307c825d89475c
maithanhtrong1113 <maithanhtrong1113@gmail.com>
2023-05-14 17:55:54 +0700

    update notShowMess

--- Components/utils/showMess.js
+++ Components/utils/showMess.js
@@ -3,7 +3,12 @@ import { useRouter } from "next/router";
 const NotShowMess = () => {
   const router = useRouter();
   const fbRoot = document.getElementById("fb-root");
-  if (router.pathname.includes("admin")) {
+  if (
+    router.pathname.includes("admin") ||
+    router.pathname.includes("cart") ||
+    router.pathname.includes("product") ||
+    router.pathname.includes("checkout")
+  ) {
     fbRoot.className = " fb_reset invisible";
   } else {
     fbRoot.className = " fb_reset";
